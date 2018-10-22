0125ceb8d98e1de4ad2bb8cc600c82a094637033
Jorge Zepeda <jorzet.94@gmail.com>
2018-10-22 12:29:38 -0500

    * change logic to show notifications

--- app/src/main/java/com/zerebrez/zerebrez/services/notification/NotificationScheduler.java
+++ app/src/main/java/com/zerebrez/zerebrez/services/notification/NotificationScheduler.java
@@ -25,12 +25,12 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Vibrator;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.TaskStackBuilder;
 
 import com.zerebrez.zerebrez.R;
 import com.zerebrez.zerebrez.ui.activities.ContentActivity;
@@ -47,11 +47,31 @@ import static com.facebook.FacebookSdk.getApplicationContext;
  */
 
 public class NotificationScheduler {
-    public static final int DAILY_REMINDER_REQUEST_CODE = 100;
-    public static final String TAG = "NotificationScheduler";
-    private static final int NOTIFICATION_ID = 2;
-    private static final String CHANNEL_ID = "zerebrez_channel_notification";
 
+    /*
+     * Tags
+     */
+    public static final String TAG = "NotificationScheduler";
+
+    /*
+     * Time variables
+     */
+    private static final int DAILY_REMINDER_REQUEST_CODE = 100;
+    private static final int DELAY_VIBRATOR = 500;
+    private static final int TIME_VIBRATOR_1 = 400;
+    private static final int TIME_VIBRATOR_2 = 300;
+
+    /*
+     * Notification variables
+     */
+    private static final int NOTIFICATION_ID = 2;
+    private static final String NOTIFICATION_CHANNEL_ID = "zerebrez_channel_notification";
+    private static CharSequence name = "ZEREBREZ";
+    private static NotificationChannel mChannel;
+
+    /*
+     * This method creates a PendingIntent with the AlarmManager according to selected time
+     */
     public static void setReminder(Context context,Class<?> cls,int hour, int min) {
 
         Calendar calendar = Calendar.getInstance();
@@ -84,6 +104,9 @@ public class NotificationScheduler {
 
     }
 
+    /*
+     * this method cancel the current PendingIntent of AlarmManager
+     */
     public static void cancelReminder(Context context,Class<?> cls) {
         // Disable a receiver
 
@@ -102,63 +125,58 @@ public class NotificationScheduler {
         pendingIntent.cancel();
     }
 
+    /*
+     * This method shows the notification
+     */
     public static void showNotification(Context context) {
 
+        // intent two vibrations with its time and delay
         Vibrator v1 = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
         assert v1 != null;
-        v1.vibrate(400);
+        v1.vibrate(TIME_VIBRATOR_1);
         try {
-            Thread.sleep(500);
+            Thread.sleep(DELAY_VIBRATOR);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         Vibrator v2 = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
         assert v2 != null;
-        v2.vibrate(300);
+        v2.vibrate(TIME_VIBRATOR_2);
 
+        // creates Uri of sound
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
         // The PendingIntent to launch our activity if the user selects this notification
         PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
                 new Intent(context, ContentActivity.class), 0);
 
-        Notification notification;
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            // Set the info for the views that show in the notification panel.
-            notification = new Notification.Builder(context)
-                    .setSmallIcon(R.drawable.main_icon)  // the status icon
-                    .setTicker("vamos a estudiar")  // the status text
-                    .setWhen(System.currentTimeMillis())  // the time stamp
-                    .setContentTitle("Zerebrez")  // the label of the entry
-                    .setContentText("Es tiempo de estudiar")  // the contents of the entry
-                    .setContentIntent(contentIntent)  // The intent to send when the entry is clicked
-                    .setChannelId(CHANNEL_ID)
-                    .build();
-        } else {
-            // Set the info for the views that show in the notification panel.
-            notification = new Notification.Builder(context)
-                    .setSmallIcon(R.drawable.main_icon)  // the status icon
-                    .setTicker("vamos a estudiar")  // the status text
-                    .setWhen(System.currentTimeMillis())  // the time stamp
-                    .setContentTitle("Zerebrez")  // the label of the entry
-                    .setContentText("Es tiempo de estudiar")  // the contents of the entry
-                    .setContentIntent(contentIntent)  // The intent to send when the entry is clicked
-                    .build();
+        // build NotificationCompat
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(getApplicationContext(),NOTIFICATION_CHANNEL_ID)
+                        .setSmallIcon(R.drawable.zerebrez_main_icon)
+                        .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.zerebrez_main_icon))
+                        .setContentTitle("Zerebrez")
+                        .setContentText("Es tiempo de estudiar")
+                        .setAutoCancel(true)
+                        .setPriority(Notification.PRIORITY_MAX)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(contentIntent);
+
+
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (notificationManager != null) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                int importance = NotificationManager.IMPORTANCE_HIGH;
+                mChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, name, importance);
+                notificationManager.createNotificationChannel(mChannel);
+            }
+
+            notificationManager.notify(NOTIFICATION_ID /* ID of notification */, notificationBuilder.build());
         }
-
-        // Send the notification.
-        NotificationManager mNM = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && mNM != null) {
-            CharSequence name = "Zerebrez";
-            int importance = NotificationManager.IMPORTANCE_HIGH;
-            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
-            mNM.createNotificationChannel(mChannel);
-        }
-
-        assert mNM != null;
-        mNM.notify(NOTIFICATION_ID, notification);
-
     }
 
 }
